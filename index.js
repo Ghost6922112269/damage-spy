b2e7fb4a9ab9016b3bb326c8ad61c6d27ca9a743
hugedong69 <pentagonalpentagon@gmail.com>
2017-10-28 17:24:34 +0800

    Release

--- index.js
+++ index.js
@@ -1,63 +1,73 @@
-var Recall, match;
-
-match = function(a, b) {
-  return (a.high === b.high) && (a.low === b.low);
-};
-
-module.exports = Recall = (function() {
-  function Recall(dispatch) {
-    var id, party;
-    party = {};
-    id = {
-      high: 0,
-      low: 0
+const Command = require('command');
+module.exports = function DamageSpy(dispatch) {
+    let flags = {
+        dps: false,
+        heal: false
     };
-    dispatch.hook('S_LOGIN', 4, function(data) {
-      id = data.cid;
-    });
-    dispatch.hook('S_EACH_SKILL_RESULT', 3, function(data) {
-      if (!match(data.target, id)) {
-        dispatch.toClient('S_EACH_SKILL_RESULT', 3, {
-          source: id,
-		  owner: id,
-          target: data.target,
-          model: 10101,
-          skill: 10100 + 0x4000000,
-          stage: 0,
-          unk1: 0,
-          id: 0,
-          time: 0,
-          damage: data.damage,
-          type: 1,
-          type2: 4
-          });
-      }
-    });
-    dispatch.hook('S_PARTY_MEMBER_LIST', 5, function(data) {
-      var i, len, member, name, ref;
-      party = {};
-      ref = data.members;
-      for (i = 0, len = ref.length; i < len; i++) {
-        member = ref[i];
-        if (party[name = member.cID.high] == null) {
-          party[name] = {};
-        }
-        party[member.cID.high][member.cID.low] = 1;
-      }
-    });
-    dispatch.hook('S_EACH_SKILL_RESULT', 3, function(data) {
-      var noct, ref;
-      if (((ref = party[data.source.high]) != null ? ref[data.source.low] : void 0) != null) {
-        if (data.type === 1) {
-          noct = +match(data.source, id);
-          data.source = id;
-          data.type2 = (data.type2 & ~4) | (noct << 2);
-        }
-      }
-      return true;
-    });
-  }
+    let pcid;
+    const command = Command(dispatch);
 
-  return Recall;
+    dispatch.hook('S_LOGIN', 4, (event) => {
+        pcid = event.cid;
+    });
 
-})();
+//Commands
+    command.add('spy', (cmd, t1, t2) => {
+
+        if (cmd === "dps") {
+            flags.dps = !flags.dps;
+            command.message(`DPS Spying: ${flags.dps}`);
+        }
+
+        if (cmd === "heal") {
+            flags.heal = !flags.heal;
+            command.message(`Healer Spying: ${flags.heal}`);
+        }
+    });
+//Do stuff
+    dispatch.hook('S_EACH_SKILL_RESULT', 3, (event) => {
+        if (flags.dps) {
+            if (event.source.equals(pcid) || event.owner.equals(pcid)) {
+                event.type2 = 0;
+                return true;
+            }
+            if ((!event.source.equals(pcid) || (!event.owner.equals(pcid) && event.owner > 0)) && !event.target.equals(pcid)) {
+                dispatch.toClient('S_EACH_SKILL_RESULT', 3, {
+                    source: pcid,
+                    owner: pcid,
+                    target: event.target,
+                    model: 10101,
+                    skill: 10100 + 0x4000000,
+                    stage: 0,
+                    unk1: 0,
+                    id: 0,
+                    time: 0,
+                    damage: event.damage,
+                    type: 1,
+                    type2: 1,
+                    crit: event.crit
+                });
+            }
+        }
+        if (flags.heal) {
+            if ((!event.source.equals(pcid) || (!event.owner.equals(pcid) && event.owner > 0)) && !event.target.equals(pcid) && event.type === 2) {
+                dispatch.toClient('S_EACH_SKILL_RESULT', 3, {
+                    source: pcid,
+                    owner: pcid,
+                    target: event.target,
+                    model: 10101,
+                    skill: 10100 + 0x4000000,
+                    stage: 0,
+                    unk1: 0,
+                    id: 0,
+                    time: 0,
+                    damage: event.damage,
+                    type: 3,
+                    type2: 1,
+                    crit: event.crit
+                });
+            };
+        }
+
+    });
+};
