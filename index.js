c54e0c305b3ebfafdd4f3f31969fba9d5e147457
hugedong69 <pentagonalpentagon@gmail.com>
2017-10-28 18:11:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,49 @@
 const Command = require('command');
 module.exports = function DamageSpy(dispatch) {
+    //Change these to toggle features on/off on startup
     let flags = {
         dps: false,
-        heal: false
+        heal: false,
+        me: false
     };
-    let pcid;
+    let pcid,
+        players = {};
     const command = Command(dispatch);
 
     dispatch.hook('S_LOGIN', 4, (event) => {
         pcid = event.cid;
     });
+    dispatch.hook('S_SPAWN_USER', 3, (event) => {
+        players[event.cid] = event;
+    });
 
-//Commands
+    dispatch.hook('S_DESPAWN_USER', 2, (event) => {
+        delete players[event.target];
+    });
+    //Commands
     command.add('spy', (cmd, t1, t2) => {
 
         if (cmd === "dps") {
             flags.dps = !flags.dps;
             command.message(`DPS Spying: ${flags.dps}`);
         }
-
+        if (cmd === "me") {
+            flags.me = !flags.me;
+            command.message(`Showing all damage: ${flags.me}`);
+        }
         if (cmd === "heal") {
             flags.heal = !flags.heal;
             command.message(`Healer Spying: ${flags.heal}`);
         }
     });
-//Do stuff
+    //Do stuff
     dispatch.hook('S_EACH_SKILL_RESULT', 3, (event) => {
         if (flags.dps) {
             if (event.source.equals(pcid) || event.owner.equals(pcid)) {
                 event.type2 = 0;
                 return true;
             }
-            if ((!event.source.equals(pcid) || (!event.owner.equals(pcid) && event.owner > 0)) && !event.target.equals(pcid)) {
+            if (players[event.source] || players[event.owner]) {
                 dispatch.toClient('S_EACH_SKILL_RESULT', 3, {
                     source: pcid,
                     owner: pcid,
@@ -46,6 +58,27 @@ module.exports = function DamageSpy(dispatch) {
                     type: 1,
                     type2: 1,
                     crit: event.crit
+                });
+            }
+        }
+        if (flags.me) {
+            if (event.source.equals(pcid) || event.owner.equals(pcid)) {
+                dispatch.toClient('S_EACH_SKILL_RESULT', 3, {
+                    source: pcid,
+                    owner: pcid,
+                    target: event.target,
+                    model: event.model,
+                    skill: event.damage,
+                    stage: event.stage,
+                    unk1: event.unk1,
+                    id: event.id,
+                    time: event.time,
+                    damage: event.damage,
+                    type: event.type,
+                    type2: event.type,
+                    crit: event.crit,
+                    blocked: 0,
+                    setTargetAction: event.setTargetAction
                 });
             }
         }
